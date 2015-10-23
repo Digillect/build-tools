fc74b56cfece0f09b34e08b2bfbf7e7a195eed20
Andrew Nefedkin <andrew.nefedkin@actis.ru>
2015-10-23 15:56:14 +0300

    Completely redesigned package structure and usage workflow.

---
+++ build.cmd
@@ -0,0 +1,37 @@
+@echo off
+
+setlocal enableextensions
+set EnableNuGetPackageRestore=true
+
+pushd %~dp0
+
+call :ResolveNuGet nuget.exe
+
+if errorlevel 1 (
+	echo NuGet.Exe was not found either in .nuget subfolder or PATH
+	exit /b %ERRORLEVEL%
+)
+
+if exist .nuget\packages.config (
+	"%NuGetExe%" restore .nuget\packages.config -PackagesDirectory packages -Verbosity quiet -NonInteractive
+)
+
+rem if exist *.sln (
+rem 	for %%f in (*.sln) do "%NuGetExe%" restore "%%f" -NonInteractive
+rem )
+
+if not errorlevel 1 (
+	%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe build.proj /nologo /v:m %*
+)
+
+popd
+goto :EOF
+
+:ResolveNuGet
+set NuGetExe=%~$PATH:1
+if not "%NuGetExe%"=="" goto :EOF
+if exist %CD%\.nuget\%1 (
+	set NuGetExe=%CD%\.nuget\%1
+	goto :EOF
+)
+exit /b 1
